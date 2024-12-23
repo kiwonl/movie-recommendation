fb955773a8a0d16bd2f35eecf2e47d33e1dcfe3d
kiwonl <61114855+kiwonl@users.noreply.github.com>
2024-12-23 09:28:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,124 +1,107 @@
+# import base64
+import vertexai
+from vertexai.generative_models import GenerativeModel, SafetySetting, Part
 import os
-from flask import Flask, request, jsonify, render_template
-from langchain_google_vertexai import VertexAI
-from langchain_google_vertexai import VertexAIEmbeddings, VertexAI
-from langchain_google_alloydb_pg import AlloyDBEngine
-from langchain_core.prompts import PromptTemplate
-from langchain_google_alloydb_pg import (
-    AlloyDBEngine,
-    AlloyDBVectorStore,
-)
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains import create_retrieval_chain
-from langchain_core.documents import Document
+# import sys
+import json
+from flask import Flask, request, render_template
+import logging
+from prometheus_client import generate_latest, REGISTRY, Counter, Gauge, Histogram
 
-project_id = os.environ["PRJ_ID"]
-instance_name = "movies-instance"
-cluster_name = "movies-cluster"
-alloy_region = "us-central1"
-alloy_user = "postgres"
-alloy_password = "movies-demo-password"
-database = "movies"
-vector_table_name = "movie_titles"
-
-llm = VertexAI(model_name="gemini-1.5-flash-001", project=project_id, max_output_tokens=2048)
-
-engine = AlloyDBEngine.from_instance(
-    project_id=project_id,
-    instance=instance_name,
-    region=alloy_region,
-    cluster=cluster_name,
-    database=database,
-    user=alloy_user,
-    password=alloy_password,
-)
-
-# Initialize the embedding service
-embeddings_service = VertexAIEmbeddings(
-    model_name="textembedding-gecko@003", project=project_id
-)
-
-vector_store = AlloyDBVectorStore.create_sync(
-    engine=engine,
-    embedding_service=embeddings_service,
-    table_name=vector_table_name,
-    metadata_columns=[
-        "show_id",
-        "type",
-        "country",
-        "date_added",
-        "release_year",
-        "duration",
-        "listed_in",
-    ],
-)
-
-prompt = PromptTemplate.from_template("""
-
-You are an expert on box office movies.
-
-Comment on the movies {movies} and how good of a fit they are for the scenario described as "{scenario}".
-
-Given the similar entries from your movie library, also provide recommendations for alternative movies.
-Make sure your additional recommendations include duration, release_year and show_id of the movies.
-
-Use all the information from the context to answer the question.
-If the context includes relevant information, include them in your recommendation.
-
-Similar movies from your movie library:
-
-```{context}
-```
-""")
-
-retriever = vector_store.as_retriever(
-    search_type="mmr", search_kwargs={"k": 5, "lambda_mult": 0.8}
-)
-
-customDocumentPrompt = PromptTemplate.from_template("""
-Result:
-
-Summary:
-{page_content}
-
-Metadata:
-show_id: {show_id}
-release_year: {release_year}
-duration: {duration}
-""")
-
-
-combine_docs_chain = create_stuff_documents_chain(
-    llm, prompt, document_prompt=customDocumentPrompt
-)
-
+# Initialize Flask app
 app = Flask(__name__)
-@app.route('/recommendations', methods=['POST'])
-def movie_recommendations():
-  """
-  Returns a list of movie recommendations based on the user's input.
-  """
-  if not request.json:
-      return jsonify({'error': 'Missing JSON payload'}), 400
+# Set werkzeug logger level to ERROR to suppress unnecessary logs
+logging.getLogger('werkzeug').setLevel(logging.ERROR)
 
-  movies = ", or".join(request.json['movies'])
-  scenario = request.json['scenario']
+# Prometheus metrics
+# Counter for total HTTP requests
+REQUESTS = Counter('http_requests_total', 'Total HTTP Requests (count)', ['method', 'endpoint', 'status_code'])
+# Gauge for number of in-progress HTTP requests
+IN_PROGRESS = Gauge('http_requests_inprogress', 'Number of in progress HTTP requests')
+# Histogram for HTTP request latency
+TIMINGS = Histogram('http_request_duration_seconds', 'HTTP request latency (seconds)')
 
-  movie_recommendations = vector_store.similarity_search(f"Movie that is great for {scenario} and similar to {movies}", k=5)
+# Initialize Vertex AI
+vertexai.init(project=os.getenv("PROJECT_ID"), location=os.getenv("REGION"))
+# Initialize Gemini model
+# model = GenerativeModel("gemini-1.5-flash-002")
+model = GenerativeModel(os.getenv("GEMINI_MODEL"))
+generation_config = {
+  "max_output_tokens": 8192,
+  "temperature": 1,
+  "top_p": 0.95,
+}
+safety_settings = [
+  SafetySetting(
+    category=SafetySetting.HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+    threshold=SafetySetting.HarmBlockThreshold.OFF
+  ),
+  SafetySetting(
+    category=SafetySetting.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+    threshold=SafetySetting.HarmBlockThreshold.OFF
+  ),
+  SafetySetting(
+    category=SafetySetting.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+    threshold=SafetySetting.HarmBlockThreshold.OFF
+  ),
+  SafetySetting(
+    category=SafetySetting.HarmCategory.HARM_CATEGORY_HARASSMENT,
+    threshold=SafetySetting.HarmBlockThreshold.OFF
+  ),
+]
 
-  response = combine_docs_chain.invoke({"scenario": scenario, "movies": movies, "context": movie_recommendations})
+# Function to get movie recommendations from Vertex AI
+def vertex_movie_recommendation(movies, scenario):
+  chat = model.start_chat()
+  response = chat.send_message(
+    f"""당신은 영화 전문가 입니다. 다음 영화들: {" or ".join(movies)} 중에서, {scenario} 상황에 적합한 영화를 추천해 주세요""",
+    generation_config=generation_config,
+    safety_settings=safety_settings,
+    stream=False
+  )
+  return response.text
 
-  return jsonify({'recommendation': response})
-
+# Route for the main page
 @app.route('/')
+@IN_PROGRESS.track_inprogress()
+@TIMINGS.time()
 def index():
-  """
-  Renders the index page.
-  """
+  # Prometheus metric for GET request to /
+  REQUESTS.labels(method='POST', endpoint="/recommendations", status_code=200).inc()
+  # Render index page with revision and region from environment variables
   return render_template('index.html', revision=os.getenv("K_REVISION"), region=os.getenv("GCP_REGION"))
 
+# Route for movie recommendations
+@app.route('/recommendations', methods=['POST'])
+@IN_PROGRESS.track_inprogress()
+@TIMINGS.time()
+def movie_recommendations():
+  movies = request.json['movies']
+  scenario = request.json['scenario']
+  print(f"Received movies: {movies}, Received scenario: {scenario}")
+
+  try:
+    recommendation_response = vertex_movie_recommendation(movies, scenario)
+  except Exception as e:
+    print(f"Error occurred: {e}")
+  
+  result = json.dumps({'recommendation': recommendation_response}, ensure_ascii = False)
+  print(f"Result: {result}")
+  
+  # Prometheus metric for POST request to /recommendations
+  REQUESTS.labels(method='POST', endpoint="/recommendations", status_code=200).inc()
+  return result, 200, {'Content-Type': 'application/json; charset=utf-8'}
+
+# Route for Prometheus metrics
+@app.route('/metrics')
+@IN_PROGRESS.track_inprogress()
+@TIMINGS.time()
+def metrics():
+    # Prometheus metric for GET request to /metrics
+    REQUESTS.labels(method='GET', endpoint="/metrics", status_code=200).inc()
+    return generate_latest(REGISTRY)
+
+# Run the Flask app
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
-    app.run(debug=True, host='0.0.0.0', port=port)
-
-
+  port = int(os.environ.get('PORT', 8080))
+  app.run(debug=False, host='0.0.0.0', port=port)
